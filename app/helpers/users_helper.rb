85c0dadd8567253eed31b0ce474a2b55de2602a0
ren <wjhmkrenren@icloud.com>
2018-02-04 15:17:18 +0000

    Make a basic User model (including secure passwords)

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,8 @@
 module UsersHelper
+    def gravatar_for(user)
+        # Digest::MD5::hexdigestで引数をハッシュ化(user.email.downcase)
+        gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+        gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
+        image_tag(gravatar_url,alt: user.name, class: "gravatar")
+    end
 end
